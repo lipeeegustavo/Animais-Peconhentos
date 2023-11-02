b443acc81f4a3ce156665a8843cd503e36d85d46
lipeeegustavo <116122814+lipeeegustavo@users.noreply.github.com>
2023-11-01 23:46:55 -0300

    ok

--- plantas.js
+++ plantas.js
@@ -1,24 +1,44 @@
 let currentSlide = 0;
 const slides = document.querySelectorAll('.slide');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
 
 function showSlide(n) {
     slides[currentSlide].style.display = 'none';
-    currentSlide = (n + slides.length) % slides.length;
+    currentSlide = n;
     slides[currentSlide].style.display = 'block';
+
+    // Desabilitar o botão "Anterior" na primeira imagem
+    if (currentSlide === 0) {
+        prevBtn.disabled = true;
+    } else {
+        prevBtn.disabled = false;
+    }
+
+    // Desabilitar o botão "Próximo" na última imagem
+    if (currentSlide === slides.length - 1) {
+        nextBtn.disabled = true;
+    } else {
+        nextBtn.disabled = false;
+    }
 }
 
 // Função para avançar e retroceder os slides
 function nextSlide() {
-    showSlide(currentSlide + 1);
+    if (currentSlide < slides.length - 1) {
+        showSlide(currentSlide + 1);
+    }
 }
 
 function prevSlide() {
-    showSlide(currentSlide - 1);
+    if (currentSlide > 0) {
+        showSlide(currentSlide - 1);
+    }
 }
 
 // Inicie o slider mostrando o primeiro slide
 showSlide(currentSlide);
 
-// Adicione botões ou eventos de clique para avançar e retroceder
-document.getElementById('prevBtn').addEventListener('click', prevSlide);
-document.getElementById('nextBtn').addEventListener('click', nextSlide);
+// Adicione eventos de clique para os botões de navegação
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
