92947b2b72b564b495c3b88e70ced8d825d19fb9
lipeeegustavo <116122814+lipeeegustavo@users.noreply.github.com>
2023-11-12 19:12:45 -0300

    ok

---
+++ Barbearia/main.js
@@ -0,0 +1,7 @@
+function Localizacao() {
+  // Substitua as coordenadas ou o endereço desejado
+  const url = `https://www.google.com/maps/search/?api=1&query=Hewer Barbearia`;
+
+  // Abre uma nova aba com a URL do Google Maps
+  window.open(url, "_blank");
+}
