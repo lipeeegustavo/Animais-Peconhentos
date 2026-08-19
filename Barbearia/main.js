0280e9d8d6d8d5797e7265e71cabfb5935288d2f
Lipe_gustav <116122814+lipeeegustavo@users.noreply.github.com>
2026-08-19 15:42:30 -0300

    chore: remove unrelated Barbearia and Imgens-Plantas folders

--- Barbearia/main.js
+++
@@ -1,7 +0,0 @@
-function Localizacao() {
-  // Substitua as coordenadas ou o endereço desejado
-  const url = `https://www.google.com/maps/search/?api=1&query=Hewer Barbearia`;
-
-  // Abre uma nova aba com a URL do Google Maps
-  window.open(url, "_blank");
-}
